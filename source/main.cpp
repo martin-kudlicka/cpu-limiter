9fc41daaf9477cf367f075c2ae8b5191120f0825
Martin <Martin@7be0990a-3169-0446-a381-039ec90e368b>
2017-11-25 17:09:52 +0000

    + version info * based on MApplication

--- source/main.cpp
+++ source/main.cpp
@@ -1,10 +1,12 @@
+#include <MkWidgets/MApplication>
 #include "mainwindow.h"
-#include <QtWidgets/QApplication>
 
 int main(int argc, char *argv[])
 {
-	QApplication a(argc, argv);
-	MainWindow w;
-	w.show();
-	return a.exec();
+  MApplication application(argc, argv);
+
+  MainWindow mainWindow;
+  mainWindow.show();
+
+  return application.exec();
 }
