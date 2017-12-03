2a253cecf9ecd7de72c9e332f11002a5a48b41ac
Martin <37011117+martin-kudlicka@users.noreply.github.com>
2017-12-03 13:03:26 +0000

    + target action options

--- source/ruleoptions.cpp
+++ source/ruleoptions.cpp
@@ -1,6 +1,8 @@
 #include "ruleoptions.h"
 
-const QString RuleOptions::Property::Condition_Status = "condition/status";
-
 const QString RuleOptions::Property::Enabled = "enabled";
 const QString RuleOptions::Property::Name    = "name";
+
+const QString RuleOptions::Property::Condition_Status = "condition/status";
+
+const QString RuleOptions::Property::Target_Action = "target/action";
