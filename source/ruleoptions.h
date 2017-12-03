fe5d71bc0168d178609b7a8326d118a960912ad0
Martin <Martin@7be0990a-3169-0446-a381-039ec90e368b>
2017-12-03 13:00:53 +0000

    + condition status options

--- source/ruleoptions.h
+++ source/ruleoptions.h
@@ -2,6 +2,7 @@
 #define RULEOPTIONS_H
 
 #include <MkCore/MSettings>
+#include <MkCore/MUuidPtr>
 
 class RuleOptions : public MSettings
 {
@@ -9,9 +10,12 @@ class RuleOptions : public MSettings
     class Property
     {
       public:
+        static const QString Condition_Status;
         static const QString Enabled;
         static const QString Name;
     };
+
+    virtual ~RuleOptions() Q_DECL_OVERRIDE Q_DECL_EQ_DEFAULT;
 };
 
 #endif
