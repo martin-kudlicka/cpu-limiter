129d5efbc97bb8cb4f195e6be4147e537c40672c
Martin <37011117+martin-kudlicka@users.noreply.github.com>
2017-12-09 11:51:19 +0000

    + selected processes model

--- source/ruleoptions.cpp
+++ source/ruleoptions.cpp
@@ -1,15 +1,63 @@
 #include "ruleoptions.h"
 
-const QString RuleOptions::Property::Enabled = "enabled";
-const QString RuleOptions::Property::Name    = "name";
+Q_DECL_CONSTEXPR QString RuleOptions::Property::Enabled           = "enabled";
+Q_DECL_CONSTEXPR QString RuleOptions::Property::Name              = "name";
 
-const QString RuleOptions::Property::Condition_Status = "condition/status";
+Q_DECL_CONSTEXPR QString RuleOptions::Property::Condition_SelectedProcesses = "condition/selectedProcesses";
+Q_DECL_CONSTEXPR QString RuleOptions::Property::Condition_Status            = "condition/status";
 
-const QString RuleOptions::Property::Target_Action   = "target/action";
-const QString RuleOptions::Property::Target_CPULimit = "target/cpuLimit";
+Q_DECL_CONSTEXPR QString RuleOptions::Property::Target_Action            = "target/action";
+Q_DECL_CONSTEXPR QString RuleOptions::Property::Target_CPULimit          = "target/cpuLimit";
+Q_DECL_CONSTEXPR QString RuleOptions::Property::Target_SelectedProcesses = "target/selectedProcesses";
 
 RuleOptions::RuleOptions(MUuidPtr &&id) : _id(qMove(id))
 {
   beginGroup("rules");
   beginGroup(id.toString());
+}
+
+QString RuleOptions::selectedProcesses(SelectedProcesses type, quintptr index)
+{
+  switch (type)
+  {
+  case SelectedProcesses::Condition:
+    beginReadArray(Property::Condition_SelectedProcesses);
+    break;
+  case SelectedProcesses::Target:
+    beginReadArray(Property::Target_SelectedProcesses);
+    break;
+  default:
+    Q_ASSERT_X(false, "RuleOptions::selectedProcesses", "switch (type)");
+    return QString();
+  }
+
+  setArrayIndex(index);
+
+  auto processName = value("name").toString();
+
+  endArray();
+
+  return processName;
+}
+
+quintptr RuleOptions::selectedProcessesSize(SelectedProcesses type)
+{
+  quintptr size;
+
+  switch (type)
+  {
+    case SelectedProcesses::Condition:
+      size = beginReadArray(Property::Condition_SelectedProcesses);
+      break;
+    case SelectedProcesses::Target:
+      size = beginReadArray(Property::Target_SelectedProcesses);
+      break;
+    default:
+      Q_ASSERT_X(false, "RuleOptions::selectedProcessesSize", "switch (type)");
+      return 0;
+  }
+
+  endArray();
+
+  return size;
 }
