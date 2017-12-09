93a463452ed43169ad730c18e4d6e6177a3ada69
Martin <37011117+martin-kudlicka@users.noreply.github.com>
2017-12-09 16:43:50 +0000

    + header

--- source/rulesmodel.h
+++ source/rulesmodel.h
@@ -20,12 +20,13 @@ class RulesModel : public QAbstractItemModel
 
     Rules _rules;
 
-    virtual int         columnCount(const QModelIndex &parent = QModelIndex())                      const Q_DECL_OVERRIDE;
-    virtual QVariant    data       (const QModelIndex &index, int role = Qt::DisplayRole)           const Q_DECL_OVERRIDE;
-    virtual QModelIndex index      (int row, int column, const QModelIndex &parent = QModelIndex()) const Q_DECL_OVERRIDE;
-    virtual bool        insertRows (int row, int count, const QModelIndex &parent = QModelIndex())        Q_DECL_OVERRIDE;
-    virtual QModelIndex parent     (const QModelIndex &child)                                       const Q_DECL_OVERRIDE;
-    virtual int         rowCount   (const QModelIndex &parent = QModelIndex())                      const Q_DECL_OVERRIDE;
+    virtual int         columnCount(const QModelIndex &parent = QModelIndex())                           const Q_DECL_OVERRIDE;
+    virtual QVariant    data       (const QModelIndex &index, int role = Qt::DisplayRole)                const Q_DECL_OVERRIDE;
+    virtual QVariant    headerData(int section, Qt::Orientation orientation, int role = Qt::DisplayRole) const Q_DECL_OVERRIDE;
+    virtual QModelIndex index      (int row, int column, const QModelIndex &parent = QModelIndex())      const Q_DECL_OVERRIDE;
+    virtual bool        insertRows (int row, int count, const QModelIndex &parent = QModelIndex())             Q_DECL_OVERRIDE;
+    virtual QModelIndex parent     (const QModelIndex &child)                                            const Q_DECL_OVERRIDE;
+    virtual int         rowCount   (const QModelIndex &parent = QModelIndex())                           const Q_DECL_OVERRIDE;
 };
 
 #endif
