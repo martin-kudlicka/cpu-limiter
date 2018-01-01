abef3a6b917e5b932552e1fc9df0fdec2bd0d30b
Martin <37011117+martin-kudlicka@users.noreply.github.com>
2018-01-01 20:09:46 +0000

    + monitor process start/end

--- source/rulemonitor.h
+++ source/rulemonitor.h
@@ -5,14 +5,20 @@
 
 class Rules;
 
-class RuleMonitor
+class RuleMonitor : public QObject
 {
+  Q_OBJECT
+
   public:
     RuleMonitor(Rules *rules);
 
   private:
     MGovernor _governor;
     Rules    *_rules;
+
+  private slots:
+    void on_processNotifier_ended  (DWORD id);
+    void on_processNotifier_started(const MProcessInfo &processInfo);
 };
 
 #endif
