0cb58c60f4310daec813f0056fccb13096b5b135
Martin <37011117+martin-kudlicka@users.noreply.github.com>
2018-01-06 16:42:30 +0000

    + monitor foreground window changes

--- source/rulemonitor.cpp
+++ source/rulemonitor.cpp
@@ -2,6 +2,7 @@
 
 #include "rules.h"
 #include <MkNotifier/MProcessNotifier>
+#include <MkCore/MWinEventInfo>
 
 RuleMonitor::RuleMonitor(Rules *rules) : _rules(rules)
 {
@@ -19,8 +20,12 @@ RuleMonitor::RuleMonitor(Rules *rules) : _rules(rules)
     }
   }
 
+  _winEventNotifier.addEvent(EVENT_SYSTEM_FOREGROUND);
+
   connect(&*mProcessNotifier, &MProcessNotifier::started, this, &RuleMonitor::on_processNotifier_started);
   connect(&*mProcessNotifier, &MProcessNotifier::ended,   this, &RuleMonitor::on_processNotifier_ended);
+
+  connect(&_winEventNotifier, &MWinEventNotifier::notify, this, &RuleMonitor::on_winEventNotifier_notify);
 }
 
 void RuleMonitor::on_processNotifier_ended(DWORD id)
@@ -108,6 +113,33 @@ void RuleMonitor::on_processNotifier_started(const MProcessInfo &processInfo)
       default:
         Q_ASSERT_X(false, "RuleMonitor::on_processNotifier_started", "switch (rule->options().status())");
         continue;
+    }
+  }
+}
+
+void RuleMonitor::on_winEventNotifier_notify(const MWinEventInfo &winEventInfo)
+{
+  auto foregroundProcess = MProcessInfo(winEventInfo.window());
+
+  for (const auto &rule : _rules->get())
+  {
+    if (rule->options().status() == RuleOptions::Status::NotRunning)
+    {
+      continue;
+    }
+    if (rule->options().state() == RuleOptions::State::Anyhow)
+    {
+      continue;
+    }
+
+    auto conditionsMet = rule->conditionsMet(foregroundProcess);
+    if (rule->active() && !conditionsMet)
+    {
+      rule->deactivate(&_governor);
+    }
+    else if (!rule->active() && conditionsMet)
+    {
+      rule->activate(&_governor);
     }
   }
 }
