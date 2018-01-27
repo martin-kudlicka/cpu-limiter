e0f69426b3beefd0351fa93c374150b6a9f9ffdc
Martin <37011117+martin-kudlicka@users.noreply.github.com>
2018-01-27 12:06:22 +0000

    + select process dialog

---
+++ source/selectprocessdialog.cpp
@@ -0,0 +1,43 @@
+#include "selectprocessdialog.h"
+
+#include <QtWidgets/QFileDialog>
+
+SelectProcessDialog::SelectProcessDialog(QWidget *parent) : QDialog(parent), _processesModel(MProcesses::Filter::ValidName | MProcesses::Filter::ValidPath)
+{
+  _ui.setupUi(this);
+
+  _ui.processesView->setModel(&_processesModel);
+
+  _ui.processesView->header()->setSectionResizeMode(static_cast<int>(MProcessesModel::Column::Id), QHeaderView::ResizeToContents);
+
+  connect(_ui.processesView->selectionModel(), &QItemSelectionModel::currentChanged, this, &SelectProcessDialog::on_processesView_currentChanged);
+}
+
+QString SelectProcessDialog::process() const
+{
+  return _ui.process->text();
+}
+
+void SelectProcessDialog::on_browse_clicked(bool checked /* false */)
+{
+  auto filePath = QFileDialog::getOpenFileName(this, tr("Select file"), QString(), "*.exe");
+  if (filePath.isNull())
+  {
+    return;
+  }
+
+  _ui.process->setText(QDir::toNativeSeparators(filePath));
+}
+
+void SelectProcessDialog::on_process_textChanged(const QString &text) const
+{
+  _ui.okButton->setEnabled(!text.isEmpty());
+}
+
+void SelectProcessDialog::on_processesView_currentChanged(const QModelIndex &current, const QModelIndex &previous) const
+{
+  auto index    = _processesModel.index(current.row(), static_cast<int>(MProcessesModel::Column::FilePath), current.parent());
+  auto filePath = _processesModel.data(index).toString();
+
+  _ui.process->setText(filePath);
+}
---
+++ source/selectprocessdialog.h
@@ -0,0 +1,27 @@
+#ifndef SELECTPOCESSDIALOG_H
+#define SELECTPOCESSDIALOG_H
+
+#include "ui_selectprocessdialog.h"
+#include <MkWidgets/MProcessesModel>
+
+class SelectProcessDialog : public QDialog
+{
+  Q_OBJECT
+
+  public:
+             SelectProcessDialog(QWidget *parent);
+    virtual ~SelectProcessDialog() Q_DECL_OVERRIDE Q_DECL_EQ_DEFAULT;
+
+    QString process() const;
+
+  private:
+    MProcessesModel _processesModel;
+    Ui::SelectProcessDialog _ui;
+
+  private slots:
+    void on_browse_clicked              (bool checked /* false */);
+    void on_process_textChanged         (const QString &text) const;
+    void on_processesView_currentChanged(const QModelIndex &current, const QModelIndex &previous) const;
+};
+
+#endif
