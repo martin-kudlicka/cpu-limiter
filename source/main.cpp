71028ecff2a21b4f4499ce4bdb2e4c01392acc5c
Martin <Martin@7be0990a-3169-0446-a381-039ec90e368b>
2018-03-12 12:52:29 +0000

    + analytics id

--- source/main.cpp
+++ source/main.cpp
@@ -1,9 +1,12 @@
 #include <MkWidgets/MApplication>
 #include "mainwindow.h"
+#include <MkAnalytics/MAnalytics>
 
 int main(int argc, char *argv[])
 {
   MApplication application(argc, argv);
+
+  mAnalytics->setId("UA-115441303-1");
 
   MainWindow mainWindow;
   mainWindow.show();
